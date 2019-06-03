b1bd177b78eb83812c78fdf6cd84ec16961bce81
Katsuki <Katsuki_Sato0130@icloud.com>
2019-06-03 21:09:33 +0900

    Add Article struct

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json: "desc"`
+	Content string `json: "content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test Title", Desc: "Test Desk", Content: "Hello World"},
+	}
+
+	fmt.Println("Endpoint Hit:All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage Endpoint Hit")
@@ -12,6 +30,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func handleRecuests() {
 	http.HandleFunc("/", home)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
