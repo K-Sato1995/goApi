3785a2b416c83fef08cf1509058482e9ac313bb0
Katsuki <Katsuki_Sato0130@icloud.com>
2019-06-03 21:03:02 +0900

    Basic Serve Function

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
 import (
-  "fmt",
-  "log",
-  "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage Endpoint Hit")
+}
+
+func handleRecuests() {
+	http.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main() {
+	handleRecuests()
+}
